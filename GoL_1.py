d78f85476964e7a505a1d4d8e059ab66c58a1365
Hy-5 <coulbaly.ismael@gmail.com>
2021-04-08 02:54:03 -0500

    commented out pycallgraph profiling

--- GoL_1.py
+++ GoL_1.py
@@ -2,9 +2,11 @@ import sys, getopt, os, copy, time, threading
 from pycallgraph import PyCallGraph
 from pycallgraph.output import GraphvizOutput
 
-with PyCallGraph(output=GraphvizOutput()):
-    code_to_profile()
 #from multiprocessing import Process
+
+#Pycallgraph integration
+'''with PyCallGraph(output=GraphvizOutput()):
+    code_to_profile()'''
 
 #Creating input matrix iMat as a list() of size l(number of rows)
 def matCreation():
